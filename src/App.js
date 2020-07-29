3d413193028e0780a5af88ce730fc0fc8ce9ccf7
Rebecca Amarh-Adjei <37591664+ramarhadjei@users.noreply.github.com>
2020-07-29 15:41:44 -0400

    first commit

---
+++ src/App.js
@@ -0,0 +1,17 @@
+import React, {Component} from 'react';
+
+
+class Game extends Component {
+  render(){
+    return(
+<div className="style">
+  <div>
+    Put the PlayerCard Components here
+  </div>
+  <div className="winner">Place the winner here</div>
+  <button type="button">Play the Game</button>
+</div>
+    )
+  }
+}
+export default Game;
